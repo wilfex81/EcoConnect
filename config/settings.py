7b65389be78bdea25171ac5c6ac07b9792a2ebd7
Wilfex <84175893+wilfex81@users.noreply.github.com>
2024-04-03 23:29:03 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
